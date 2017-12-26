98432989d1475d98823b49e93eaaead72e2a452e
VTGroup <infor.vtgroup@gmail.com>
2017-12-26 20:00:44 +0700

    new update

--- asset/js/html5-video-playlist.js
+++ asset/js/html5-video-playlist.js
@@ -1,7 +1,7 @@
 /**
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Donate link: http://vt-group.vn/donate.html
+* License: GPLv2 or later
+* License URI: http://www.gnu.org/licenses/gpl-2.0.html
+* Donate link: http://vt-group.vn/donate.html
 **/
 (function($){
 	"use strict"
